4fb2ebf1adf6d1bbee53d10815eaa881c64a1232
Dear Bear <dearerbear@gmail.com>
2018-08-02 21:26:47 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,6 @@
 # out the current state.
 
 def display_board(board)
-  board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
   line = "-----------"
   puts "#{board[0]}|#{board[1]}|#{board[2]}"
   puts line
