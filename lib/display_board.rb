f466f8f97b68445bd715fce639a71bb51cc4e45a
Dear Bear <dearerbear@gmail.com>
2018-08-02 21:25:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,16 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-#def display_board
-#  row_input = ["   ", "|", " x  ", "|", "   "]
-#  array_row = row_input.join { |a| print a }
-#  row = String.new(row_input[0] + row_input[1] + row_input[2] + row_input[3] + row_input[4])
-#  line = "-----------"
-#  puts array_row
-#  puts line
-#  puts array_row
-#  puts line
-#  puts array_row
-#end
 
 def display_board(board)
   board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
